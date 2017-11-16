842155ac9773529061d341e68fdbc88613727399
Adam Hansen <adamlars90@gmail.com>
2017-11-16 13:33:06 +0100

    Added configuration files

--- DigitalThesisRegistration/Startup.cs
+++ DigitalThesisRegistration/Startup.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
@@ -13,17 +14,34 @@ namespace DigitalThesisRegistration
 {
     public class Startup
     {
-        public Startup(IConfiguration configuration)
-        {
-            Configuration = configuration;
-        }
+        private IHostingEnvironment Environment { get; }
 
-        public IConfiguration Configuration { get; }
+        private IConfiguration Configuration { get; }
+
+        public Startup(IHostingEnvironment env)
+        {
+            var builder = new ConfigurationBuilder()
+                .SetBasePath(env.ContentRootPath)
+                .AddJsonFile("appsettings.json", false, true)
+                .AddEnvironmentVariables();
+            Configuration = builder.Build();
+            Environment = env;
+        }
 
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddMvc();
+
+            // Configure context
+            services.AddSingleton(Configuration);
+            //if (Environment.IsDevelopment())
+            //    services.AddDbContext<VolunteerRegistrationContext>(opt => opt.UseInMemoryDatabase("VR"));
+            //else
+            //    services.AddDbContext<VolunteerRegistrationContext>(opt => opt.UseSqlServer(Configuration.GetConnectionString("DefaultConnection")));
+
+            // Add Dependencies
+            //services.AddScoped<IUnitOfWork, UnitOfWork>();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -33,6 +51,12 @@ namespace DigitalThesisRegistration
             {
                 app.UseDeveloperExceptionPage();
             }
+
+            // Add cors for Angular
+            app.UseCors(builder => builder.WithOrigins("http://localhost:4200")
+                .AllowAnyMethod()
+                .AllowAnyHeader()
+                .AllowCredentials());
 
             app.UseMvc();
         }
