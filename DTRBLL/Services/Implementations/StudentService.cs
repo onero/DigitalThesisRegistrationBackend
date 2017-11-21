873dbcb28c3fadd951925969ca345244159fa51e
Adam Hansen <adamlars90@gmail.com>
2017-11-21 13:35:23 +0100

    Added Student Service (create)

---
+++ DTRBLL/Services/Implementations/StudentService.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections.Generic;
+using DTRBLL.BusinessObjects;
+using DTRBLL.Converters.Implementations;
+using DTRDAL.UOW;
+
+namespace DTRBLL.Services.Implementations
+{
+    internal class StudentService : IStudentService
+    {
+        private readonly IUnitOfWork _uow;
+        private readonly StudentConverter _converter;
+
+        public StudentService(IUnitOfWork uow)
+        {
+            _uow = uow;
+            _converter = new StudentConverter();
+        }
+        public StudentBO Create(StudentBO bo)
+        {
+            using (var unitOfWork = _uow)
+            {
+                var convertedEntity = _converter.Convert(bo);
+                var createdEntity = unitOfWork.StudentRepository.Create(convertedEntity);
+                unitOfWork.Complete();
+                return _converter.Convert(createdEntity);
+            }
+        }
+
+        public StudentBO Get(int id)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public IList<StudentBO> GetAll()
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public bool Delete(int id)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public StudentBO Update(StudentBO bo)
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
---
+++ DTRBLLTests/Implementations/StudentServiceShould.cs
@@ -0,0 +1,72 @@
+﻿using DTRBLL.BusinessObjects;
+using DTRBLL.Services;
+using DTRBLL.Services.Implementations;
+using DTRDAL.Entities;
+using DTRDAL.Repositories;
+using DTRDAL.UOW;
+using Moq;
+using Xunit;
+
+namespace DTRBLLTests.Implementations
+{
+    public class StudentServiceShould : IServiceTest
+    {
+        private readonly Mock<IUnitOfWork> _uow = new Mock<IUnitOfWork>();
+        private readonly Mock<IStudentRepository> _repo = new Mock<IStudentRepository>();
+        private readonly IStudentService _service;
+        
+
+        public StudentServiceShould()
+        {
+            _uow.SetupGet(r => r.StudentRepository).Returns(_repo.Object);
+            _service = new StudentService(_uow.Object);
+        }
+
+
+        [Fact]
+        public void CreateOne()
+        {
+            _repo.Setup(r => r.Create(It.IsAny<Student>())).Returns(new Student());
+
+            var entity = _service.Create(new StudentBO());
+
+            Assert.NotNull(entity);
+
+        }
+        [Fact]
+        public void GetOneByExistingId()
+        {
+            throw new System.NotImplementedException();
+        }
+        [Fact]
+        public void NotGetOneByNonExistingId()
+        {
+            throw new System.NotImplementedException();
+        }
+        [Fact]
+        public void GetAll()
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public void DeleteByExistingId()
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public void NotDeleteByNonExistingId()
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public void UpdateByExistingId()
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public void NotUpdateByNonExistingId()
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
