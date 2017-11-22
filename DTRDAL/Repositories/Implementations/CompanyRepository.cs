fafe3a990f58542a16649166b48516bc7b46e0b9
Mathias Sejrup Plougmann <mathias_j93@hotmail.com>
2017-11-22 14:04:00 +0100

    Company DAL
    Ready for implementation

---
+++ DTRDAL/Repositories/Implementations/CompanyRepository.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using DTRDAL.Context;
+using DTRDAL.Entities;
+
+namespace DTRDAL.Repositories.Implementations
+{
+    class CompanyRepository : ICompanyRepository
+    {
+        private readonly DTRContext _context;
+
+        public CompanyRepository(DTRContext context)
+        {
+            _context = context;
+        }
+
+        public Company Create(Company ent)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Company Get(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IEnumerable<Company> GetAll()
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool Delete(int id)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ DTRDALTests/Implementations/CompanyRepositoryShould.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using DTRDAL.Context;
+using DTRDAL.Repositories;
+using DTRDAL.Repositories.Implementations;
+using DTRDALTests.implementations;
+
+namespace DTRDALTests.Implementations
+{
+    class CompanyRepositoryShould : IRepositoryTest
+    {
+        private readonly DTRContext _context;
+        private readonly CompanyRepository _repository;
+
+        public CompanyRepositoryShould()
+        {
+            _context = TestContext.Context;
+            _repository = new CompanyRepository(_context);
+        }
+
+        public void CreateOne()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void GetOneByExistingId()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void NotGetOneByNonExistingId()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void GetAll()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void DeleteByExistingId()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void NotDeleteByNonExistingId()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
