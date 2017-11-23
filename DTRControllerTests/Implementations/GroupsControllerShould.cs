45d90327bb9b1c6dced22829157bb85f6f8f3903
Adam Hansen <adamlars90@gmail.com>
2017-11-23 10:57:23 +0100

    Implemented GroupsController: Get

---
+++ DTRControllerTests/Implementations/GroupsControllerShould.cs
@@ -0,0 +1,96 @@
+﻿using System.Collections.Generic;
+using DigitalThesisRegistration.Controllers;
+using DTRBLL.BusinessObjects;
+using DTRBLL.Services;
+using Microsoft.AspNetCore.Mvc;
+using Moq;
+using Xunit;
+
+namespace DTRControllerTests.Implementations
+{
+    public class GroupsControllerShould : IControllerTest
+    {
+        private readonly Mock<IGroupService> _service = new Mock<IGroupService>();
+        private readonly GroupsController _controller;
+
+        public GroupsControllerShould()
+        {
+            _controller = new GroupsController(_service.Object);
+        }
+
+        [Fact]
+        public void GetAll()
+        {
+            _service.Setup(r => r.GetAll()).Returns(new List<GroupBO>{new GroupBO()});
+
+            var result = _controller.Get();
+
+            Assert.NotNull(result);
+            Assert.NotEmpty(result);
+        }
+        [Fact]
+        public void GetByExistingId()
+        {
+            _service.Setup(r => r.Get(It.IsAny<int>())).Returns(new GroupBO());
+
+            var result = _controller.Get(1);
+            Assert.IsType<OkObjectResult>(result);
+        }
+        [Fact]
+        public void NotGetByNonExistingId_ReturnNotFound()
+        {
+            var result = _controller.Get(0);
+            Assert.IsType<NotFoundObjectResult>(result);
+        }
+        [Fact]
+        public void PostWithValidObject()
+        {
+            throw new System.NotImplementedException();
+        }
+        [Fact]
+        public void NotPostWithInvalidObject_ReturnBadRequest()
+        {
+            throw new System.NotImplementedException();
+        }
+        [Fact]
+        public void NotPostWithNull_ReturnBadRequest()
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public void DeleteByExistingId_ReturnOk()
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public void NotDeleteByNonExistingId_ReturnNotFound()
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public void UpdateWithValidObject_ReturnOk()
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public void NotUpdateWithNull_ReturnBadRequest()
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public void NotUpdateWithMisMatchingIds_ReturnBadRequest()
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public void NotUpdateWithInvalidObject_ReturnBadRequest()
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public void NotUpdateWithNonExistingId_ReturnNotFound()
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
---
+++ DigitalThesisRegistration/Controllers/GroupsController.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using DTRBLL.BusinessObjects;
+using DTRBLL.Services;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace DigitalThesisRegistration.Controllers
+{
+    [Produces("application/json")]
+    [Route("api/Groups")]
+    public class GroupsController : Controller
+    {
+        private readonly IGroupService _service;
+
+        public GroupsController(IGroupService service)
+        {
+            _service = service;
+        }
+
+        // GET: api/Groups
+        [HttpGet]
+        public IEnumerable<GroupBO> Get()
+        {
+            return _service.GetAll();
+        }
+
+        // GET: api/Groups/5
+        [HttpGet("{id}", Name = "GetGroup")]
+        public IActionResult Get(int id)
+        {
+            var result = _service.Get(id);
+            if (result == null)
+            {
+                return new NotFoundObjectResult(result);
+            }
+            return new OkObjectResult(result);
+        }
+
+        // POST: api/Groups
+        [HttpPost]
+        public void Post([FromBody]string value)
+        {
+        }
+        
+        // PUT: api/Groups/5
+        [HttpPut("{id}")]
+        public IActionResult Put(int id, [FromBody]GroupBO value)
+        {
+            throw new NotImplementedException();
+        }
+
+        // DELETE: api/ApiWithActions/5
+        [HttpDelete("{id}")]
+        public IActionResult Delete(int id)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
