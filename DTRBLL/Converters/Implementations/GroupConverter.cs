8917f9ca60ef130fe86505861376cde521823c35
Adam Hansen <adamlars90@gmail.com>
2017-11-23 10:09:36 +0100

    Implemented GroupConverter

---
+++ DTRBLL/Converters/Implementations/GroupConverter.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using DTRBLL.BusinessObjects;
+using DTRDAL.Entities;
+
+namespace DTRBLL.Converters.Implementations
+{
+    public class GroupConverter : IConverter<Group, GroupBO>
+    {
+
+        private readonly StudentConverter _studentConverter = new StudentConverter();
+        public Group Convert(GroupBO bo)
+        {
+            if (bo == null) return null;
+            return new Group
+            {
+                Id = bo.Id,
+                Name = bo.Name,
+                ContactEmail = bo.ContactEmail,
+                Students = bo?.Students.Select(_studentConverter.Convert).ToList()
+            };
+        }
+
+        public GroupBO Convert(Group entity)
+        {
+            if (entity == null) return null;
+            return new GroupBO
+            {
+                Id = entity.Id,
+                Name = entity.Name,
+                ContactEmail = entity.ContactEmail,
+                Students = entity.Students?.Select(_studentConverter.Convert).ToList(),
+                StudentIds = entity.Students?.Select(s => s.Id).ToList()
+            };
+        }
+    }
+}
