881ae003aee76327fe72690ae629a57176e729cf
Adam Hansen <adamlars90@gmail.com>
2017-11-27 11:09:41 +0100

    Implemented Controller Contract: Get & Post

--- DTRBLL/BusinessObjects/ContractBO.cs
+++ DTRBLL/BusinessObjects/ContractBO.cs
@@ -1,9 +1,14 @@
-﻿namespace DTRBLL.BusinessObjects
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace DTRBLL.BusinessObjects
 {
     public class ContractBO
     {
+        [Required]
         public int GroupId { get; set; }
+        [Required]
         public int CompanyId { get; set; }
+        [Required]
         public int ProjectId { get; set; }
         public bool IsApproved { get; set; }
     }
