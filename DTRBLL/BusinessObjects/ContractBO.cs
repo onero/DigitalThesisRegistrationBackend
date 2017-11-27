72c6a59aeca0988d6bc40e382e592b657f35e216
Adam Hansen <adamlars90@gmail.com>
2017-11-27 10:15:57 +0100

    Implemented Contract BLL: ContractConverter

---
+++ DTRBLL/BusinessObjects/ContractBO.cs
@@ -0,0 +1,10 @@
+﻿namespace DTRBLL.BusinessObjects
+{
+    public class ContractBO
+    {
+        public int GroupId { get; set; }
+        public int CompanyId { get; set; }
+        public int ProjectId { get; set; }
+        public bool IsApproved { get; set; }
+    }
+}
