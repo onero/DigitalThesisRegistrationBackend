92648983c7191f8c948ca3d5596ca49d12b6bff8
Adam Hansen <adamlars90@gmail.com>
2017-12-01 14:06:56 +0100

    Added Swagger

--- DigitalThesisRegistration/Startup.cs
+++ DigitalThesisRegistration/Startup.cs
@@ -8,6 +8,7 @@ using Microsoft.AspNetCore.Hosting;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using Swashbuckle.AspNetCore.Swagger;
 
 namespace DigitalThesisRegistration
 {
@@ -49,6 +50,12 @@ namespace DigitalThesisRegistration
             services.AddScoped<ISupervisorService, SupervisorService>();
             services.AddScoped<IProjectService, ProjectService>();
             services.AddScoped<IContractService, ContractService>();
+
+            // Register the Swagger generator, defining one or more Swagger documents
+            services.AddSwaggerGen(c =>
+            {
+                c.SwaggerDoc("v1", new Info { Title = "My API", Version = "v1" });
+            });
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -62,6 +69,16 @@ namespace DigitalThesisRegistration
                 .AllowAnyMethod()
                 .AllowAnyHeader()
                 .AllowCredentials());
+
+            // Enable middleware to serve generated Swagger as a JSON endpoint.
+            app.UseSwagger();
+
+            // Enable middleware to serve swagger-ui (HTML, JS, CSS, etc.), specifying the Swagger JSON endpoint.
+            app.UseSwaggerUI(c =>
+            {
+                c.SwaggerEndpoint("/swagger/v1/swagger.json", "My API V1");
+            });
+
 
             app.UseMvc();
         }
