9cc08b44107f13c50cbc46ea0a9acb867b36a8db
Adam Hansen <adamlars90@gmail.com>
2017-12-04 10:25:01 +0100

    Implemented Projects: Swagger documentation

--- DigitalThesisRegistration/Controllers/ProjectsController.cs
+++ DigitalThesisRegistration/Controllers/ProjectsController.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using DigitalThesisRegistration.Helpers;
 using DTRBLL.BusinessObjects;
 using DTRBLL.Services;
 using Microsoft.AspNetCore.Http;
@@ -20,6 +21,10 @@ namespace DigitalThesisRegistration.Controllers
             _service = service;
         }
 
+        /// <summary>
+        /// GET all projects
+        /// </summary>
+        /// <returns>Collection of ProjectBOs</returns>
         // GET: api/AssignedProjects
         [HttpGet]
         public IEnumerable<ProjectBO> Get()
@@ -27,6 +32,11 @@ namespace DigitalThesisRegistration.Controllers
             return _service.GetAll();
         }
 
+        /// <summary>
+        /// GET a project by id
+        /// </summary>
+        /// <param name="id"></param>
+        /// <returns>ProjectBO, if id exists</returns>
         // GET: api/AssignedProjects/5
         [HttpGet("{id}", Name = "GetProject")]
         public IActionResult Get(int id)
@@ -34,29 +44,70 @@ namespace DigitalThesisRegistration.Controllers
             var result = _service.Get(id);
             if (result == null)
             {
-                return NotFound();
+                return new NotFoundObjectResult(ErrorMessages.NotFoundString);
             }
             return new OkObjectResult(result);
         }
-        
+
+        /// <summary>
+        /// POST a new project
+        /// </summary>
+        /// <param name="value"></param>
+        /// <returns>Created ProjectBO, if correct format is used</returns>
+        /// <remarks>
+        /// Sample request:
+        ///
+        ///     POST api/Projects
+        ///     {
+        /// 
+        ///     }
+        ///
+        /// </remarks>
         // POST: api/AssignedProjects
         [HttpPost]
         public IActionResult Post([FromBody]ProjectBO value)
         {
-            if (value == null) return BadRequest();
+            if (value == null) return new BadRequestObjectResult(ErrorMessages.InvalidEntityString);
             if (!ModelState.IsValid) return new BadRequestObjectResult(ModelState);
             return new OkObjectResult(_service.Create(value));
         }
-        
+
+        /// <summary>
+        /// PUT updated information on a project by id
+        /// </summary>
+        /// <param name="id"></param>
+        /// <param name="value"></param>
+        /// <returns>Updated ProjectBO, if correct format is used</returns>
+        /// <remarks>
+        /// Sample request:
+        ///
+        ///     PUT api/Projects/1
+        ///     {
+        ///        "id": 1,
+        ///        "description": "Description,
+        ///        "start": "2017-12-19T23:00:00",
+        ///        "end": "2018-01-19T23:00:00",
+        ///        "title": "Digital Thesis Registration",
+        ///        "wantedSupervisorId": 1,
+        ///        "assignedSupervisorId": 1
+        ///     }
+        ///
+        /// Validation performed upon request:
+        /// 
+        ///     - entity cannot be null, returns BadRequestResult
+        ///     - id must match id of provided entity, returns BadRequestResult
+        ///     - The [Required] attributes of entity must be provided, returns BadRequestResult
+        ///     - id of entity must exist, returns NotFoundObjectResult
+        /// </remarks>
         // PUT: api/AssignedProjects/5
         [HttpPut("{id}")]
         public IActionResult Put(int id, [FromBody]ProjectBO value)
         {
-            if (value == null) return new BadRequestResult();
-            if (id != value.Id) return new BadRequestResult();
+            if (value == null) return new BadRequestObjectResult(ErrorMessages.InvalidEntityString);
+            if (id != value.Id) return new BadRequestObjectResult(ErrorMessages.MismatchingIdString);
             if (!ModelState.IsValid) return new BadRequestObjectResult(ModelState);
             var result = _service.Update(value);
-            if (result == null) return new NotFoundObjectResult(result);
+            if (result == null) return new NotFoundObjectResult(ErrorMessages.NotFoundString);
             return new OkObjectResult(result);
         }
         
