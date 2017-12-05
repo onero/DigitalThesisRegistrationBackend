12d949d5046e1aa649b8c1284d37a058a1b74e81
Adam Hansen <adamlars90@gmail.com>
2017-12-05 13:11:27 +0100

    Implemented Appendix: DAL

---
+++ DTRDAL/Entities/Appendix.cs
@@ -0,0 +1,8 @@
+﻿namespace DTRDAL.Entities
+{
+    public class Appendix
+    {
+        public string Condition { get; set; }
+        public string Resources { get; set; }
+    }
+}
---
+++ DTRDAL/Repositories/IAppendixRepository.cs
@@ -0,0 +1,20 @@
+﻿using DTRDAL.Entities;
+
+namespace DTRDAL.Repositories
+{
+    public interface IAppendixRepository
+    {
+        /// <summary>
+        /// Load Appendix from file
+        /// </summary>
+        /// <returns>Appendix</returns>
+        Appendix Load();
+
+        /// <summary>
+        /// Save Appendix to file
+        /// </summary>
+        /// <param name="appendix">Appendix to save</param>
+        /// <returns>True, if saved correctly</returns>
+        bool Save(Appendix appendix);
+    }
+}
