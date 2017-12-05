3e4d3cb7cb7f3a1642fd79f9ea88f910b09d5487
Adam Hansen <adamlars90@gmail.com>
2017-12-05 13:37:39 +0100

    Implemented Appendix: BLL

---
+++ DTRBLL/BusinessObjects/AppendixBO.cs
@@ -0,0 +1,8 @@
+﻿namespace DTRBLL.BusinessObjects
+{
+    public class AppendixBO
+    {
+        public string Condition { get; set; }
+        public string Resources { get; set; }
+    }
+}
---
+++ DTRBLL/Services/IAppendixService.cs
@@ -0,0 +1,20 @@
+﻿using DTRBLL.BusinessObjects;
+
+namespace DTRBLL.Services
+{
+    public interface IAppendixService
+    {
+        /// <summary>
+        /// Load Appendix from file
+        /// </summary>
+        /// <returns>Appendix</returns>
+        AppendixBO Load();
+
+        /// <summary>
+        /// Save Appendix to file
+        /// </summary>
+        /// <param name="appendix">Appendix to save</param>
+        /// <returns>True, if saved correctly</returns>
+        bool Save(AppendixBO appendix);
+    }
+}
