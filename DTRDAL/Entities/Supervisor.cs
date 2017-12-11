34400c97f0c144f24b58ce008271f1cd917a93c4
Adam Hansen <adamlars90@gmail.com>
2017-12-11 12:53:06 +0100

    Upgraded Group and Supervisor
    Added UserId

--- DTRDAL/Entities/Supervisor.cs
+++ DTRDAL/Entities/Supervisor.cs
@@ -6,6 +6,7 @@ namespace DTRDAL.Entities
 {
     public class Supervisor : Person
     {
+        public int UserId { get; set; }
         public List<Project> AssignedProjects { get; set; }
         public List<Project> WantedProjects { get; set; }
     }
