2f6304b3450d07a9a2bf02f610da9cc4fdfed25e
Adam Hansen <adamlars90@gmail.com>
2017-12-12 11:16:57 +0100

    Updated documentation

--- DTRBLL/Services/IUserService.cs
+++ DTRBLL/Services/IUserService.cs
@@ -8,9 +8,10 @@ namespace DTRBLL.Services
         /// <summary>
         /// Create a new user in DB
         /// </summary>
-        /// <param name="userBO"></param>
-        /// <param name="userDBBO"></param>
-        /// <returns></returns>
+        /// <param name="userBO">User input with username to create</param>
+        /// <param name="userDBBO">UserDB input with passwordHash and Salt</param>
+        /// <returns>Created user</returns>
+        /// <remarks>If User role is "Group" the new group is created</remarks>
         User Create(UserBO userBO, UserDBBO userDBBO);
     }
 }
